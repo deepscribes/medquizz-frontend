6004a28b568818e3c0e55158aa52be4cd0eee537
nicolello-dev <nicolamigone179@gmail.com>
2024-05-07 16:12:24 +0200

    Questions now load randomly and are guaranteed to have their answers

--- src/app/api/getQuestions/route.ts
+++ src/app/api/getQuestions/route.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient, Question } from "@prisma/client";
+import { Answer, Prisma, PrismaClient, Question } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -10,20 +10,38 @@ export async function GET(req: NextRequest) {
     biologia: 23,
     chimica: 15,
     "fisica e matematica": 13,
-  };
-  const res: Question[] = [];
-  for (const [subject, count] of Object.entries(subjectQuestions)) {
-    const questions = await client.question.findMany({
+  } as const;
+  const res: (Question & { answers: Answer[] })[] = [];
+  for (const subject of Object.keys(subjectQuestions)) {
+    const totalQuestions = await client.question.count({
       where: {
         subject,
+        answers: {
+          some: {
+            isCorrect: true,
+          },
+        },
       },
-      take: count,
-      orderBy: {
-        number: "asc",
+    });
+    const questions = await client.question.findMany({
+      where: {
+        AND: {
+          subject,
+          answers: {
+            some: {
+              isCorrect: true,
+            },
+          },
+        },
       },
+      take: subjectQuestions[subject as keyof typeof subjectQuestions],
       include: {
         answers: true,
       },
+      skip: Math.floor(
+        Math.random() * totalQuestions -
+          subjectQuestions[subject as keyof typeof subjectQuestions]
+      ),
     });
     res.push(...questions);
   }
