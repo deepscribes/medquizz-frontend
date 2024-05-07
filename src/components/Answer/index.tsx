6d42f109d4f04e4ac3979b27131af9a9ffba0383
nicolello-dev <nicolamigone179@gmail.com>
2024-05-08 00:40:09 +0200

    Unfucked answers

--- src/components/Answer/index.tsx
+++ src/components/Answer/index.tsx
@@ -16,14 +16,14 @@ function ReviewAnswer({
 }) {
   return (
     <div
-      className={`flex flex-row gap-x-4 items-center p-2 sm:p-4 border-cardborder ${
+      className={`flex flex-row align-middle justify-center items-center gap-x-4 p-2 sm:p-4 border-cardborder ${
         isCorrect && "bg-[#E6F7E6] border-[#34B634] text-white"
       } ${
         selected && !isCorrect && "bg-[#FDD2D2] border-[#FA4343] text-black"
       } border rounded-md`}
     >
       <div
-        className={`flex w-8 h-8 sm:w-12 text-center justify-center items-center font-bold text-lg sm:text-xl border border-cardborder ${
+        className={`inline-flex w-8 h-8 sm:w-12 sm:h-12 text-center justify-center items-center font-bold text-lg sm:text-xl border border-cardborder ${
           isCorrect && "bg-[#34B634]"
         } ${
           selected && !isCorrect && "bg-[#FA4343] text-white"
@@ -31,22 +31,24 @@ function ReviewAnswer({
       >
         {answerChar}
       </div>
-      <p
-        className="flex-shrink-[3] text-black"
-        dangerouslySetInnerHTML={{ __html: insertImageInText(answer.text) }}
-      ></p>
-      <p className="text-black mr-0 ml-auto font-semibold">
-        {selected && isCorrect
-          ? "+1.5" // Selected answer is correct
-          : selected && !isCorrect
-          ? "-0.4" // Selected answer is wrong
-          : ""}
-        {
-          isBlank &&
-            isCorrect &&
-            "0" /* If the answer was left blank, put a "0" in the correct answer */
-        }
-      </p>
+      <div className="inline-flex flex-1 w-full flex-row justify-between items-center gap-x-1">
+        <p
+          className="text-base text-black"
+          dangerouslySetInnerHTML={{ __html: insertImageInText(answer.text) }}
+        ></p>
+        <p className="text-black font-semibold">
+          {selected && isCorrect
+            ? "+1.5" // Selected answer is correct
+            : selected && !isCorrect
+            ? "-0.4" // Selected answer is wrong
+            : ""}
+          {
+            isBlank &&
+              isCorrect &&
+              "0" /* If the answer was left blank, put a "0" in the correct answer */
+          }
+        </p>
+      </div>
     </div>
   );
 }
@@ -80,21 +82,19 @@ export function Answer({
   return (
     <>
       <div
-        className={`flex flex-row gap-x-4 items-center p-2 sm:p-4 ${
-          selected ? "bg-[#E0F2FF]" : "bg-[#F7F7F7]"
-        } ${
+        className={`p-2 sm:p-4 ${selected ? "bg-[#E0F2FF]" : "bg-[#F7F7F7]"} ${
           selected ? "border-primary" : "border-[#9D9D9D]"
         } border rounded-md`}
       >
         <div
-          className={`flex flex-grow w-8 h-8 sm:w-12 sm:h-12 text-center justify-center items-center font-bold text-lg sm:text-xl ${
+          className={`inline-flex mr-4 flex-grow w-8 h-8 sm:w-12 sm:h-12 text-center justify-center items-center font-bold text-lg sm:text-xl ${
             selected ? "bg-primary text-white" : "bg-white text-black"
           } ${!selected && "border"} border-cardborder rounded-md capitalize`}
         >
           {answerChar}
         </div>
         <p
-          className="flex-shrink-[3] text-base"
+          className="inline text-base"
           dangerouslySetInnerHTML={{ __html: answer.text }}
         ></p>
       </div>
