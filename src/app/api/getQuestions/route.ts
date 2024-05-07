c98b09b58ea254b2905c8d195f20cc775420e85c
nicolello-dev <nicolamigone179@gmail.com>
2024-05-07 06:04:15 +0200

    Questions are now weighted properly

--- src/app/api/getQuestions/route.ts
+++ src/app/api/getQuestions/route.ts
@@ -1,17 +1,32 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, Question } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const client = new PrismaClient();
 
-  const res = await client.question.findMany({
-    include: {
-      answers: true,
-    },
-    take: 60,
-    orderBy: {
-      question: "asc",
-    },
-  });
+  const subjectQuestions = {
+    "competenze di lettura e conoscenze acquisite negli studi": 4,
+    "ragionamento logico e problemi": 5,
+    biologia: 23,
+    chimica: 15,
+    "fisica e matematica": 13,
+  };
+  const res: Question[] = [];
+  for (const [subject, count] of Object.entries(subjectQuestions)) {
+    const questions = await client.question.findMany({
+      where: {
+        subject,
+      },
+      take: count,
+      orderBy: {
+        number: "asc",
+      },
+      include: {
+        answers: true,
+      },
+    });
+    res.push(...questions);
+  }
+
   return NextResponse.json(res);
 }
