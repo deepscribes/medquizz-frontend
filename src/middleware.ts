4c243f8fae5ae8a2f83222f72f0ef12da054df6f
nicolello-dev <nicolamigone179@gmail.com>
2024-05-23 13:31:21 +0200

    redirect URL is now changed in the various stages

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,21 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/risultati(.*)"]);
 
+function getRedirectUrl() {
+  if (process.env.VERCEL_ENV === "development") {
+    return "http://localhost:3000/sign-in";
+  }
+  if (process.env.VERCEL_ENV === "preview") {
+    return "https://medquizz-frontend-git-feature-deepscribes.vercel.app/sign-in";
+  }
+  return "https://medquizz.it/sign-in";
+}
+
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req))
     auth().protect({
-      unauthorizedUrl: "http://localhost:3000/sign-in",
-      unauthenticatedUrl: "http://localhost:3000/sign-in",
+      unauthorizedUrl: getRedirectUrl(),
+      unauthenticatedUrl: getRedirectUrl(),
     });
 });
 
