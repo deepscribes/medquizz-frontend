5624cda69e141f238a7c11bc18c374aceffaba8d
nicolello-dev <nicolamigone179@gmail.com>
2024-06-27 18:08:46 +0200

    Add new consent test with real data

--- src/__test__/api/consent.test.ts
+++ src/__test__/api/consent.test.ts
@@ -1,12 +1,30 @@
 import type { Consent } from "@/types/consent";
 import { pushConsent } from "@/lib/consent";
 
-test.skip("Generate new consent", async () => {
+test.skip("Generate new consent with mock email and phone number", async () => {
   const mockConsent: Consent = {
     email: "mock@test.com",
     number: "+15555550100",
     first_name: "Mock",
     last_name: "User",
+    proofs: [
+      {
+        content: "Mock proof HTML",
+        form: "<p>Mock proof</p>",
+      },
+    ],
+  };
+
+  const result = await pushConsent(mockConsent);
+  expect(result.status).toBe(200);
+});
+
+test.skip("Generate new consent with real email and phone number", async () => {
+  const mockConsent: Consent = {
+    email: "nicolamigone179@gmail.com",
+    number: "+393388267756",
+    first_name: "Nicola",
+    last_name: "Migone",
     proofs: [
       {
         content: "Mock proof HTML",
