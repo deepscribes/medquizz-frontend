fd7eb05b28303bdd34b6da093a506d2760f147f5
nicolello-dev <nicolamigone179@gmail.com>
2024-07-10 23:10:25 +0200

    Update setTimeout timeout

--- src/app/commenti/page.tsx
+++ src/app/commenti/page.tsx
@@ -101,7 +101,7 @@ export default function Commenti() {
     }
     if (timer) clearTimeout(timer);
 
-    setTimer(setTimeout(updateExplanation, 1000));
+    setTimer(setTimeout(updateExplanation, 2000));
 
     if (!userId && !(subject == Subject.Chimica && number == 1)) {
       setShowModal(true);
