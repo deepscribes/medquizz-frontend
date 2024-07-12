812f35081a103d6fbe9f3d2916b59331f501f68a
nicolello-dev <nicolamigone179@gmail.com>
2024-07-12 02:02:49 +0200

    Fixed build errors

--- src/components/home/FAQ/index.tsx
+++ src/components/home/FAQ/index.tsx
@@ -8,9 +8,9 @@ const FAQQuestions = [
         <p>Facile! È possibile avere i quesiti commentati in due modi:</p>
         <ul className="list-decimal ml-4">
           <li>
-            Se vuoi consultarli in stile "PDF", clicca su Prova Gratis! e scorri
-            fino alla sezione Extra per{" "}
-            <a href="/commenti">accedere ai Quesiti commentati dall'AI.</a>
+            Se vuoi consultarli in stile &quot;PDF&quot;, clicca su Prova
+            Gratis! e scorri fino alla sezione Extra per{" "}
+            <a href="/commenti">accedere ai Quesiti commentati dall&apos;AI.</a>
           </li>
           <li>
             Dopo aver completato una simulazione, clicca per consegnare la
