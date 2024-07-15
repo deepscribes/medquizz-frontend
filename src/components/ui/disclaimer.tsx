45c658a660c6148daae2899c94490d2e7c6687e5
nicolello-dev <nicolamigone179@gmail.com>
2024-07-15 11:15:12 +0200

    Fix build error

--- src/components/ui/disclaimer.tsx
+++ src/components/ui/disclaimer.tsx
@@ -4,8 +4,8 @@ export function Disclaimer() {
       <p className="mx-auto my-8 px-8 text-center md:w-1/2">
         <span className="font-bold">Disclaimer</span>: Seguiamo le fonti
         ufficiali del MUR. Anche se una risposta potrebbe essere errata,
-        riportiamo quella indicata dal MUR. L'IA potrebbe fornire spiegazioni
-        con variazioni per chiarire eventuali discrepanze.
+        riportiamo quella indicata dal MUR. L&apos;IA potrebbe fornire
+        spiegazioni con variazioni per chiarire eventuali discrepanze.
       </p>
     </>
   );
