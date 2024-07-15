47cbeaccd51fc30067b6a8b3e1a3d263eac275d7
nicolello-dev <nicolamigone179@gmail.com>
2024-07-15 11:18:17 +0200

    Update disclaimer's colors and size

--- src/components/ui/disclaimer.tsx
+++ src/components/ui/disclaimer.tsx
@@ -1,7 +1,7 @@
 export function Disclaimer() {
   return (
     <>
-      <p className="mx-auto my-8 px-8 text-center md:w-1/2">
+      <p className="mx-auto text-sm text-text-gray my-8 px-8 text-center md:w-1/2">
         <span className="font-bold">Disclaimer</span>: Seguiamo le fonti
         ufficiali del MUR. Anche se una risposta potrebbe essere errata,
         riportiamo quella indicata dal MUR. L&apos;IA potrebbe fornire
