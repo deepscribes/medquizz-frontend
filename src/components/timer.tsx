0d1ed7be530fbf3f52d89b9ff2c8643e55f84e40
nicolello-dev <nicolamigone179@gmail.com>
2024-07-16 23:57:32 +0200

    Fix timer bug by which the time went down by an hour if negative

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 function msToHMS(ms: number) {
   // 1- Convert to seconds:
-  var seconds = ms / 1000;
+  var seconds = Math.abs(ms) / 1000;
   // 2- Extract hours:
   var hours = Math.floor(seconds / 3600); // 3,600 seconds in 1 hour
   seconds = seconds % 3600; // seconds remaining after extracting hours
@@ -10,9 +10,9 @@ function msToHMS(ms: number) {
   var minutes = Math.floor(seconds / 60); // 60 seconds in 1 minute
   // 4- Keep only seconds not extracted to minutes:
   seconds = Math.floor(seconds % 60);
-  return `${(hours * 60 + minutes).toString().padStart(2, "0")}m ${seconds
+  return `${ms < 0 ? "-" : ""}${(hours * 60 + minutes)
     .toString()
-    .padStart(2, "0")}s`;
+    .padStart(2, "0")}m ${seconds.toString().padStart(2, "0")}s`;
 }
 
 export function Timer({
