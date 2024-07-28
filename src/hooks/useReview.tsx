abc220adb697e9837c345414a1eeab62dccb9e6c
Nicola Migone <nicolamigone179@gmail.com>
2024-07-29 00:01:47 +0200

    Remove unused functions

--- src/hooks/useReview.tsx
+++ src/hooks/useReview.tsx
@@ -13,30 +13,6 @@ export enum ReviewType {
   AfterTest,
 }
 
-function reviewTypeToString(review: ReviewType): string {
-  switch (review) {
-    case ReviewType.False:
-      return "false";
-    case ReviewType.DuringTest:
-      return "during";
-    case ReviewType.AfterTest:
-      return "after";
-  }
-}
-
-function stringToReviewType(review: string): ReviewType {
-  switch (review) {
-    case "false":
-      return ReviewType.False;
-    case "during":
-      return ReviewType.DuringTest;
-    case "after":
-      return ReviewType.AfterTest;
-    default:
-      return ReviewType.False;
-  }
-}
-
 export const ReviewContext = createContext<{
   review: ReviewType;
   setReview: React.Dispatch<React.SetStateAction<ReviewType>>;
