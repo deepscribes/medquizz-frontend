0ab846dd85c189360cfe4fe0186372fa7152f31b
Nicola Migone <nicolamigone179@gmail.com>
2024-10-16 16:39:27 +0200

    Fix auth

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,12 +2,11 @@
 
 import * as React from "react";
 import { SignIn } from "@clerk/nextjs";
-import { Container } from "@/components/ui/container";
 
 export default function Page() {
   return (
-    <Container className="max-w-none">
+    <div className="w-full flex flex-row justify-center">
       <SignIn />
-    </Container>
+    </div>
   );
 }
